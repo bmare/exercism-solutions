b9c36e428ef276a6ff8cf6649cb0dce3520b6c0a
bmare <brandon.marrow@gmail.com>
2020-01-11 14:23:38 -0500

    added second iteration of scrabble_score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,8 +1,11 @@
 def score(word):
-    total=0
-    score_dict={("A", "E", "I", "O", "U", "L","N", "R", "S", "T"): 1, ("D","G"):2, ("B","C","M","P"):3,("F","H","V","W","Y"):4, ("F","H","V","W","Y"):4, ("K"):5, ("J","X"):8, ("Q","Z"):10} 
-    for ch in word.upper():
-        for key in score_dict.keys():
-            if ch in key:
-                total+=score_dict.get(key,0)
-    return total
+    letter_sets=[("A", "E", "I", "O", "U", "L","N", "R", "S", "T"),
+            ("D","G"),
+            ("B","C","M","P"),
+            ("F","H","V","W","Y"),
+            ("K"), ("J","X"), ("Q","Z")]
+    values=[1,2,3,4,5,8,10]
+    score_dict={letter_sets[x][y]:values[x] \
+            for x in range(len(letter_sets)) \
+            for y in range(len(letter_sets[x]))} 
+    return sum(score_dict.get(ch,0) for ch in word.upper())
